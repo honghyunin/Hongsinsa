8d5e95976c8f9d1f48410440bd66465911e2e10e
honghyunin <hyunin0102@gmail.com>
2023-01-29 00:50:42 +0900

    ADD | Coupon Entity 작성

---
+++ src/main/kotlin/commerce/hosinsa/HosinsaApplication.kt
@@ -0,0 +1,11 @@
+package commerce.hosinsa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HosinsaApplication
+
+fun main(args: Array<String>) {
+	runApplication<HosinsaApplication>(*args)
+}
---
+++ src/main/kotlin/commerce/hosinsa/domain/coupon/entity/Coupon.kt
@@ -0,0 +1,35 @@
+package commerce.hosinsa.domain.coupon.entity
+
+import commerce.hosinsa.domain.member.entity.Member
+import javax.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+class Coupon(
+
+    @Column(name = "name", nullable = false)
+    val name: String? = null,
+
+    @Column(name = "discount", nullable = false)
+    val discount: Short? = null,
+
+    @Column(name = "assigned_at", nullable = false)
+    val assignedAt: LocalDateTime? = LocalDateTime.now(),
+
+    @Column(name = "expired_at", nullable = false)
+    val expiredAt: LocalDateTime? = null,
+
+    @Column(name = "status", nullable = false)
+    val status: Char, // E(expired or Valid)
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    val member: Member
+
+) {
+
+    @Column(name = "coupon_id", nullable = false)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val couponId: Int? = null
+
+}
