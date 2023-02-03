31aa29302d52aae3029e46c57cae1b9e1775bd67
honghyunin <hyunin0102@gmail.com>
2023-02-03 22:33:47 +0900

    ADD | brandAvailable, auditAvailable, brandUpdate 테스트 작성

---
+++ src/test/kotlin/commerce/hosinsa/domain/brand/service/BrandServiceTest.kt
@@ -0,0 +1,85 @@
+package commerce.hosinsa.domain.brand.service
+
+import commerce.hosinsa.domain.brand.fixtures.NAME
+import commerce.hosinsa.domain.brand.fixtures.getBrandAvailableDto
+import commerce.hosinsa.domain.brand.fixtures.getBrandUpdateDto
+import commerce.hosinsa.global.exception.CustomException
+import commerce.hosinsa.global.exception.ErrorCode.BRAND_NOT_FOUND
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.mockk.*
+
+internal class BrandServiceTest : DescribeSpec({
+    describe("brandAvailable") {
+        context("존재하지 않는 브랜드가 입력되면") {
+
+            every { brandService.brandAvailable(brandAvailableDto) } just Runs
+
+            it("브랜드 입점 심사를 등록한다") {
+                brandService.brandAvailable(brandAvailableDto)
+
+                verify(exactly = 1) { brandService.brandAvailable(brandAvailableDto) }
+            }
+        }
+
+        context("이미 존재하는 브랜드가 입력되면") {
+            every { brandService.brandAvailable(brandAvailableDto) } throws CustomException(BRAND_NOT_FOUND)
+
+            it("브랜드 입점 심사 등록에 실패한다") {
+                shouldThrow<CustomException> {
+                    brandService.brandAvailable(brandAvailableDto)
+                }
+            }
+        }
+    }
+
+    describe("auditAvailable") {
+
+        context("입점 기준에 부합하는 브랜드가 입력되면") {
+
+            every { brandService.auditAvailable(NAME) } just Runs
+
+            it("입점 심사에 통과한다") {
+                brandService.auditAvailable(NAME)
+
+                verify(exactly = 1) { brandService.auditAvailable(NAME) }
+            }
+        }
+        context("입점 기준에 부합하지 않을 경우") {
+
+            every { brandService.auditAvailable(NAME) } throws CustomException(BRAND_NOT_FOUND)
+
+            it("입점 심사에 탈락한다") {
+                shouldThrow<CustomException> { brandService.auditAvailable(NAME) }
+            }
+        }
+    }
+
+    describe("brandUpdate") {
+
+        context("존재하는 브랜드 정보를 입력받을 경우") {
+            every { brandService.brandUpdate(brandUpdateDto) } just Runs
+
+            it("브랜드 정보 업데이트에 성공한다") {
+                brandService.brandUpdate(brandUpdateDto)
+
+                verify(exactly = 1) { brandService.brandUpdate(brandUpdateDto) }
+            }
+        }
+
+        context("존재하지 않는 브랜드를 입력받을 경우") {
+            every { brandService.brandUpdate(brandUpdateDto) } throws CustomException(BRAND_NOT_FOUND)
+
+            it("브랜드 정보 업데이트에 실패한다") {
+                shouldThrow<CustomException> { brandService.brandUpdate(brandUpdateDto) }
+            }
+        }
+    }
+}) {
+
+    companion object {
+        val brandAvailableDto = getBrandAvailableDto()
+        val brandUpdateDto = getBrandUpdateDto()
+        val brandService = mockk<BrandService>()
+    }
+}
--- src/test/kotlin/commerce/hosinsa/domain/member/service/MemberServiceTest.kt
+++ src/test/kotlin/commerce/hosinsa/domain/member/service/MemberServiceTest.kt
@@ -1,142 +1,138 @@
 package commerce.hosinsa.domain.member.service
 
-import commerce.hosinsa.domain.member.dto.PWChangeDto
 import commerce.hosinsa.domain.member.fixtures.*
 import commerce.hosinsa.domain.member.repository.MemberRepository
-import commerce.hosinsa.global.config.utils.pwMatches
 import commerce.hosinsa.global.config.utils.toMember
 import commerce.hosinsa.global.exception.CustomException
 import commerce.hosinsa.global.exception.ErrorCode.*
 import io.kotest.assertions.throwables.shouldThrow
-import io.kotest.core.extensions.Extension
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.*
 
-internal class MemberServiceTest : DescribeSpec() {
+internal class MemberServiceTest : DescribeSpec({
 
-    init {
-        this.describe("signUp") {
-            context("올바른 회원 정보가 입력되면") {
-                every { memberRepository.existsById(signUpDto.id) } returns false
-                every { memberService.signUp(signUpDto) } just Runs
+    describe("signUp") {
+        context("올바른 회원 정보가 입력되면") {
+            every { memberRepository.existsById(signUpDto.id) } returns false
+            every { memberService.signUp(signUpDto) } just Runs
 
-                memberService.signUp(signUpDto)
+            memberService.signUp(signUpDto)
 
-                every { memberRepository.findById(signUpDto.id) } returns member
+            every { memberRepository.findById(signUpDto.id) } returns member
 
-                val findMember = memberRepository.findById(signUpDto.id)!!
+            val findMember = memberRepository.findById(signUpDto.id)!!
 
-                it("회원가입이 성공한다") {
-                    signUpDto.id shouldBe findMember.id
-                    signUpDto.email shouldBe findMember.email
-                }
-            }
-
-            context("이미 존재하는 회원 정보가 입력되면") {
-                every { memberRepository.existsById(signUpDto.id) } returns true
-
-                every { memberService.signUp(signUpDto) } throws CustomException(USER_ALREADY_EXISTS)
-
-                it("회원가입에 실패한다") {
-                    shouldThrow<CustomException> {
-                        memberService.signUp(signUpDto)
-                    }
-                }
+            it("회원가입이 성공한다") {
+                signUpDto.id shouldBe findMember.id
+                signUpDto.email shouldBe findMember.email
             }
         }
 
-        this.describe("signIn") {
-            context("올바른 회원 정보가 입력되면") {
-                every { memberService.signIn(signInDto) } returns tokenResponse
+        context("이미 존재하는 회원 정보가 입력되면") {
+            every { memberRepository.existsById(signUpDto.id) } returns true
 
-                val token = memberService.signIn(signInDto)
+            every { memberService.signUp(signUpDto) } throws CustomException(USER_ALREADY_EXISTS)
 
-                it("로그인이 성공한다") {
-                    token shouldBe tokenResponse
+            it("회원가입에 실패한다") {
+                shouldThrow<CustomException> {
+                    memberService.signUp(signUpDto)
                 }
-            }
-
-            context("회원정보가") {
-
-                context("존재하지 않을 경우") {
-
-                    every { memberService.signIn(signInDto) } throws CustomException(USER_NOT_FOUND)
-
-                    it("로그인에 실패한다") {
-                        failSignIn()
-                    }
-                }
-
-                context("일치하지 않을 경우") {
-
-                    every { memberService.signIn(signInDto) } throws CustomException(PASSWORD_NOT_MATCH)
-
-                    it("로그인에 실패한다") {
-                        failSignIn()
-                    }
-                }
-            }
-        }
-
-        this.describe("profileUpdate") {
-
-            context("올바른 회원 정보가 입력되면") {
-
-                every { memberService.profileUpdate(profileUpdateDto) } just Runs
-
-                it("회원정보가 변경된다")
-
-                memberService.profileUpdate(profileUpdateDto)
-
-                verify(exactly = 1) { memberService.profileUpdate(profileUpdateDto) }
-            }
-
-            context("올바르지 않은 회원 정보가 입력되면") {
-                every { memberService.profileUpdate(profileUpdateDto) } throws CustomException(USER_NOT_FOUND)
-
-                it("회원정보 변경에 실패한다") {
-                    shouldThrow<CustomException> {
-                        memberService.profileUpdate(profileUpdateDto)
-                    }
-                }
-            }
-        }
-
-        this.describe("pwChange") {
-
-            context("올바른 패스워드가 입력되면") {
-                every { memberService.pwChange(pwChangeDto) } returns CHANGED_PW
-
-                val pw = memberService.pwChange(pwChangeDto)
-
-                it("패스워드가 변경된다") {
-                    CHANGED_PW shouldBe pw
-                }
-            }
-
-            context("신규 패스워드와 재입력 비밀번호가 같지 않을 때") {
-                every { memberService.pwChange(notMatchPWChangeDto) } throws CustomException(CHANGE_PASSWORD_NOT_MATCH)
-                it("패스워드가 변경에 실패한다") {
-                    shouldThrow<CustomException> {
-                        memberService.pwChange(notMatchPWChangeDto)
-                    }
-                }
-            }
-
-            context("기존 패스워드가 일치하지 않을 때") {
-                every { memberService.pwChange(notMatchCurrentPWChangeDto) } throws CustomException(CHANGE_PASSWORD_NOT_MATCH)
-
-                it("패스워드가 변경에 실패한다") {
-                    shouldThrow<CustomException> {
-                        memberService.pwChange(notMatchCurrentPWChangeDto)
-                    }
-                }
-
             }
         }
     }
 
+    describe("signIn") {
+        context("올바른 회원 정보가 입력되면") {
+            every { memberService.signIn(signInDto) } returns tokenResponse
+
+            it("로그인이 성공한다") {
+                val token = memberService.signIn(signInDto)
+
+                token shouldBe tokenResponse
+            }
+        }
+
+        context("회원정보가") {
+
+            context("존재하지 않을 경우") {
+
+                every { memberService.signIn(signInDto) } throws CustomException(USER_NOT_FOUND)
+
+                it("로그인에 실패한다") {
+                    failSignIn()
+                }
+            }
+
+            context("일치하지 않을 경우") {
+
+                every { memberService.signIn(signInDto) } throws CustomException(PASSWORD_NOT_MATCH)
+
+                it("로그인에 실패한다") {
+                    failSignIn()
+                }
+            }
+        }
+    }
+
+    describe("profileUpdate") {
+
+        context("올바른 회원 정보가 입력되면") {
+
+            every { memberService.profileUpdate(profileUpdateDto) } just Runs
+
+            it("회원정보가 변경된다") {
+                memberService.profileUpdate(profileUpdateDto)
+
+                verify(exactly = 1) { memberService.profileUpdate(profileUpdateDto) }
+            }
+        }
+
+        context("올바르지 않은 회원 정보가 입력되면") {
+            every { memberService.profileUpdate(profileUpdateDto) } throws CustomException(USER_NOT_FOUND)
+
+            it("회원정보 변경에 실패한다") {
+                shouldThrow<CustomException> {
+                    memberService.profileUpdate(profileUpdateDto)
+                }
+            }
+        }
+    }
+
+    describe("pwChange") {
+
+        context("올바른 패스워드가 입력되면") {
+            every { memberService.pwChange(pwChangeDto) } returns CHANGED_PW
+
+            it("패스워드가 변경된다") {
+                val pw = memberService.pwChange(pwChangeDto)
+
+                CHANGED_PW shouldBe pw
+            }
+        }
+
+        context("신규 패스워드와 재입력 비밀번호가 같지 않을 때") {
+            every { memberService.pwChange(notMatchPWChangeDto) } throws CustomException(CHANGE_PASSWORD_NOT_MATCH)
+
+            it("패스워드가 변경에 실패한다") {
+                shouldThrow<CustomException> {
+                    memberService.pwChange(notMatchPWChangeDto)
+                }
+            }
+        }
+
+        context("기존 패스워드가 일치하지 않을 때") {
+
+            every { memberService.pwChange(notMatchCurrentPWChangeDto) } throws CustomException(CHANGE_PASSWORD_NOT_MATCH)
+
+            it("패스워드가 변경에 실패한다") {
+                shouldThrow<CustomException> {
+                    memberService.pwChange(notMatchCurrentPWChangeDto)
+                }
+            }
+        }
+    }
+}) {
     companion object {
         val signUpDto = getSignUpDto()
         val signInDto = getSignInDto()
@@ -152,5 +148,4 @@ internal class MemberServiceTest : DescribeSpec() {
         val memberRepository = mockk<MemberRepository>()
         val memberService = mockk<MemberServiceImpl>()
     }
-
 }
