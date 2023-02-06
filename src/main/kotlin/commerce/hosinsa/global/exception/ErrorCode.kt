d3d76eb329d5002fdf8ccfc70bc916a07efe61c8
honghyunin <hyunin0102@gmail.com>
2023-02-06 20:52:17 +0900

    UPD | ErrorCode Http Status 변경

--- src/main/kotlin/commerce/hosinsa/global/exception/ErrorCode.kt
+++ src/main/kotlin/commerce/hosinsa/global/exception/ErrorCode.kt
@@ -7,16 +7,18 @@ enum class ErrorCode(val status: Int, val msg: String) {
     PRODUCT_IS_ALREADY_EXISTS(409, "Product Already Exists"),
 
     // Brand Exception
-    BRAND_NOT_FOUND(400, "Brand Not Found"),
-    BRAND_ALREADY_EXISTS(400, "Brand Already Exists"),
+    BRAND_NOT_FOUND(404, "Brand Not Found"),
+    BRAND_ALREADY_EXISTS(409, "Brand Already Exists"),
+
     // User Exception
-    USER_NOT_FOUND(400, "User Not Found"),
-    USER_ALREADY_EXISTS(400, "User Already Exists"),
+    USER_NOT_FOUND(404, "User Not Found"),
+    USER_ALREADY_EXISTS(409, "User Already Exists"),
 
     CHANGE_PASSWORD_NOT_MATCH(400, "New Password and RePassword Do Not Match"),
+
     // Authentication Exception
     PASSWORD_NOT_MATCH(400, "Password Does Not Match"),
 
     // IO Exception
-    INPUT_VALUE_NOT_FOUND(400, "Input Value Not Found")
+    INPUT_VALUE_NOT_FOUND(404, "Input Value Not Found")
 }
