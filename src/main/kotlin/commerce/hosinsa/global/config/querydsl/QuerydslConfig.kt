3b41fd54f6f8a5edb371674f54f9fb837adc90c9
honghyunin <hyunin0102@gmail.com>
2023-02-07 18:55:33 +0900

    ADD | QuerydslConfig 추가

---
+++ src/main/kotlin/commerce/hosinsa/global/config/querydsl/QuerydslConfig.kt
@@ -0,0 +1,14 @@
+package commerce.hosinsa.global.config.querydsl
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+
+@Configuration
+class QuerydslConfig(@PersistenceContext val em: EntityManager) {
+
+    @Bean
+    fun jpaQueryFactory() = JPAQueryFactory(em)
+}
