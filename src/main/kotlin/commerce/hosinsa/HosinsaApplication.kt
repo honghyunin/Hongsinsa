d9c47b68f3a4bb29023d9211f746efd55e076bf1
honghyunin <hyunin0102@gmail.com>
2023-02-13 17:44:11 +0900

    ADD | Batch 의존성 추가

--- src/main/kotlin/commerce/hosinsa/HosinsaApplication.kt
+++ src/main/kotlin/commerce/hosinsa/HosinsaApplication.kt
@@ -1,10 +1,14 @@
 package commerce.hosinsa
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
+@EnableBatchProcessing
 class HosinsaApplication
 
 fun main(args: Array<String>) {
