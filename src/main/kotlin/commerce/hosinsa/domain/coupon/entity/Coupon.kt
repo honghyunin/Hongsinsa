d580e38c5dc7fc1c7086f95082df929217da6114
honghyunin <hyunin0102@gmail.com>
2023-02-22 17:11:34 +0900

    CHA | Coupon에 있던 개별적으로 적용시켜주어야 하는 정보를 CouponMember로 이동
    CouponMember는 발급된 쿠폰을 의미하기 때문에 이 엔티티에 인증 정보가 있어야 함, Coupon에 있을 경우 해당 쿠폰을 발급 받은 모든 Member가 다른 시간에 발급받아도 유효기간을 공유하여 변경함

--- src/main/kotlin/commerce/hosinsa/domain/coupon/entity/Coupon.kt
+++ src/main/kotlin/commerce/hosinsa/domain/coupon/entity/Coupon.kt
@@ -13,18 +13,6 @@ class Coupon(
     @Column(name = "discount", nullable = false)
     val discount: Short,
 
-    @Column(name = "assigned_at", nullable = false)
-    val assignedAt: LocalDateTime? = null,
-
-    @Column(name = "expired_at", nullable = false)
-    var expiredAt: LocalDateTime? = null,
-
-    @Column(name = "status", nullable = false)
-    var status: Char, // E(expired or Valid),
-
-    @Column(name = "is_delete", nullable = false)
-    var isDelete: Boolean = false,
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "brand_id", nullable = true)
     var brand: Brand? = null
--- src/main/kotlin/commerce/hosinsa/domain/coupon/entity/CouponMember.kt
+++ src/main/kotlin/commerce/hosinsa/domain/coupon/entity/CouponMember.kt
@@ -1,6 +1,7 @@
 package commerce.hosinsa.domain.coupon.entity
 
 import commerce.hosinsa.domain.member.entity.Member
+import java.time.LocalDateTime
 import javax.persistence.*
 
 @Entity
@@ -18,4 +19,16 @@ class CouponMember(
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "coupon_id")
     val coupon: Coupon,
+
+    @Column(name = "assigned_at", nullable = false)
+    val assignedAt: LocalDateTime? = null,
+
+    @Column(name = "expired_at", nullable = false)
+    var expiredAt: LocalDateTime? = null,
+
+    @Column(name = "status", nullable = false)
+    var status: Char, // E(expired or Valid),
+
+    @Column(name = "is_delete", nullable = false)
+    var isDelete: Boolean = false,
 )
