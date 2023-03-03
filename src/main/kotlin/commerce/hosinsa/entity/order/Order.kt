01694b52cf807e32a4f228cd0b78b351c5b05483
honghyunin <hyunin0102@gmail.com>
2023-03-03 20:32:29 +0900

    UPD | phoneNumber Column 추가, isDelete 기본 변수로 변경

--- src/main/kotlin/commerce/hosinsa/entity/order/Order.kt
+++ src/main/kotlin/commerce/hosinsa/entity/order/Order.kt
@@ -3,28 +3,25 @@ package commerce.hosinsa.entity.order
 import commerce.hosinsa.entity.BaseTimeEntity
 import commerce.hosinsa.entity.member.Member
 import commerce.hosinsa.entity.product.Product
+import org.hibernate.annotations.Comment
 import javax.persistence.*
 
 @Entity
 class Order(
-    @Column(name = "total_price", nullable = false)
-    val totalPrice: Int,
-
     @Column(name = "address", nullable = false)
     val address: String,
 
+    @Comment(value = "수령인")
     @Column(name = "recipient", nullable = false)
     val recipient: String,
 
+    @Comment(value = "연락처")
+    @Column(name = "phone_number", nullable = false)
+    val phoneNumber: String,
+
+    @Comment(value = "요청사항")
     @Column(name = "order_request", nullable = true)
-    val orderRequest: String? = null,
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id")
-    val product: Product,
-
-    @Column(name = "is_delete", nullable = false)
-    val isDelete: Boolean = false,
+    val requestedMessage: String? = null,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
@@ -36,4 +33,6 @@ class Order(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val idx: Int? = null
 
+    @Column(name = "is_delete", nullable = false)
+    var isDelete: Boolean = false
 }
