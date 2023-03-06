d62671f498d02f8af977207baa017c8bc94b023f
honghyunin <hyunin0102@gmail.com>
2023-03-06 22:37:18 +0900

    UPD | 상품 사이즈, 색상, 추가

--- src/main/kotlin/commerce/hosinsa/entity/order/OrderProduct.kt
+++ src/main/kotlin/commerce/hosinsa/entity/order/OrderProduct.kt
@@ -2,6 +2,7 @@ package commerce.hosinsa.entity.order
 
 import commerce.hosinsa.entity.BaseTimeEntity
 import commerce.hosinsa.entity.product.Product
+import commerce.hosinsa.entity.product.ProductSize
 import org.hibernate.annotations.Comment
 import javax.persistence.*
 
@@ -18,10 +19,19 @@ class OrderProduct(
 
     @Comment(value = "상품 수량")
     @Column(name = "count", nullable = false)
-    val count: Byte
+    val count: Byte,
+
+    @Column(name = "size")
+    val size: ProductSize,
+
+    @Column(name = "color")
+    val color: String,
 ): BaseTimeEntity() {
     @Column(name = "idx", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     val idx: Int? = null
+
+    @Column(name = "is_delete", nullable = false)
+    var isDelete: Boolean = false
 }
