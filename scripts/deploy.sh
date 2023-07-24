c10467d66c21931f97d3f3bffe883edada8d234e
hyunin <hyunin0102@gmail.com>
2023-07-25 00:42:30 +0900

    fix: deploy shell

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,6 +26,4 @@ echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-nohup java -jar \
-  -Dspring.config.location=classpath:/application.yml
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
