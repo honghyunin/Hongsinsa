19b44afe17b91e1f80d23c98fb9b7f50cb36269e
hyunin <hyunin0102@gmail.com>
2023-07-24 21:48:48 +0900

    feat: cd set

--- src/main/kotlin/commerce/hongsinsa/HongsinsaApplication.kt
+++ src/main/kotlin/commerce/hongsinsa/HongsinsaApplication.kt
@@ -1,8 +1,10 @@
 package commerce.hongsinsa
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
+import org.springframework.boot.context.ApplicationPidFileWriter
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
@@ -12,5 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling
 class HongsinsaApplication
 
 fun main(args: Array<String>) {
-    runApplication<HongsinsaApplication>(*args)
+    val application = SpringApplication(HongsinsaApplication::class.java)
+    application.addListeners(ApplicationPidFileWriter())
+    application.run(*args)
 }
