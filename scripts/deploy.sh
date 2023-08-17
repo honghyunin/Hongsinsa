a93d5ae12dd15e449f58611f4544e0f330f9a787
hyunin <hyunin0102@gmail.com>
2023-08-17 14:04:19 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -31,4 +31,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
-echo "> nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 & "
+echo "> nohup java -jar \
+-Dspring.config.location=classpath:/application.yml
+$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
