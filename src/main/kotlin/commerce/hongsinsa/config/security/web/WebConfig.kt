959dc0e77d433e881e67bc18b9abf05d1642cb87
hyunin <hyunin0102@gmail.com>
2023-09-04 17:08:28 +0900

    setting: server ip

--- src/main/kotlin/commerce/hongsinsa/config/security/web/WebConfig.kt
+++ src/main/kotlin/commerce/hongsinsa/config/security/web/WebConfig.kt
@@ -1,5 +1,6 @@
 package commerce.hongsinsa.config.security.web
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
@@ -10,9 +11,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfig: WebMvcConfigurer {
 
+    @Value("\${deploy.port}")
+    private var SERVER_IP: String? = null
+
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:8080", "http://52.79.226.98:8080")
+            .allowedOrigins("http://localhost:8080", "http://$SERVER_IP:8080")
             .allowedMethods(
                 HttpMethod.GET.name,
                 HttpMethod.HEAD.name,
